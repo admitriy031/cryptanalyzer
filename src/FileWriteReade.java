baf9ceda7e32d1e0164cf6cf79b0a69aa11c58be
”Dmitry <admitriy031@gmail.com>
2023-06-21 15:11:22 +0300

    added class FileWriteReade

---
+++ src/FileWriteReade.java
@@ -0,0 +1,62 @@
+import java.io.ByteArrayOutputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.nio.Buffer;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+
+public class FileWriteReade {
+    private String fileRead;
+    private String fileWrite;
+    public FileWriteReade(String fileRead, String fileWrite){
+        this.fileRead=fileRead;
+        this.fileWrite=fileWrite;
+    }
+    public byte[] readFile(){
+        try(RandomAccessFile randomAccessFile=new RandomAccessFile(fileRead, "rw");
+            FileChannel channel=randomAccessFile.getChannel();
+            ByteArrayOutputStream byteArrayOutputStream=new ByteArrayOutputStream()) {
+            ByteBuffer bytebuffer=ByteBuffer.allocate(2048);
+            int endOfFile=-1;
+            int intbufer=channel.read(bytebuffer);
+            while (endOfFile!=intbufer){
+                bytebuffer.flip();
+                while (bytebuffer.hasRemaining()) {
+                    byteArrayOutputStream.write(bytebuffer.get());
+                }
+                channel.write(bytebuffer);
+                bytebuffer.clear();
+                intbufer = channel.read(bytebuffer);
+            }
+            byte[] data = byteArrayOutputStream.toByteArray();
+            return data;
+            } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    public void writeFile(byte[] data){
+        try(RandomAccessFile randomAccessFile=new RandomAccessFile(fileWrite, "wr");
+            FileChannel channel=randomAccessFile.getChannel()){
+            ByteBuffer bytebuffer=ByteBuffer.allocate(2048);
+            int index = 0;
+            while (index < data.length) {
+                bytebuffer.clear();
+
+                while (bytebuffer.hasRemaining() && index < data.length) {
+                    bytebuffer.put(data[index++]);
+                }
+
+                bytebuffer.flip();
+                channel.write(bytebuffer);
+                bytebuffer.clear();
+            }
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
