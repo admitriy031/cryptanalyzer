5b63b90e581bc9c6e41ad0b3708c36e0f4bc53fe
”Dmitry <admitriy031@gmail.com>
2023-06-21 13:38:26 +0300

    in class Menu added: -Info Menu - method fileRead -method fileWrite

---
+++ src/FileNotFountException.java
@@ -0,0 +1,5 @@
+public class FileNotFountException extends RuntimeException{
+    public FileNotFountException(String message){
+        super(message);
+    }
+}
--- src/Menu.java
+++ src/Menu.java
@@ -1,3 +1,5 @@
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.Scanner;
 
 public class Menu {
@@ -41,4 +43,31 @@ public class Menu {
     private void itemToExit(){
         isRunning=false;
     }
+
+    private String fileRead(){
+        Scanner readScanner=new Scanner(System.in);
+        System.out.println("Enter the file path from which you want to read the text");
+        String file=readScanner.nextLine();
+        try {
+            while (isRunning){
+                isRunning=false;
+                if(Files.exists(Path.of(file))){
+                    return file;
+                }
+                else {
+                    System.out.println("Sorry, file not found");
+                }
+            }
+        }catch (FileNotFountException e){
+            System.out.println("try again");
+        }
+        throw new FileNotFountException("File path could not be validated");
+    }
+    private String fileWrite(){
+        Scanner writeScanner=new Scanner(System.in);
+        System.out.println("Enter the file in which you want to write date");
+        String file=writeScanner.nextLine();
+        return file;
+    }
 }
+
